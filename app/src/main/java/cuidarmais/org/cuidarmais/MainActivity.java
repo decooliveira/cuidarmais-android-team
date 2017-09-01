9d6b6e0cb0e6770cd65379a7f75ce132de97d150
Deco Oliveira <deco.oliveira@googlemail.com>
2017-09-01 14:53:55 -0300

    new fragments

--- app/src/main/java/cuidarmais/org/cuidarmais/MainActivity.java
+++ app/src/main/java/cuidarmais/org/cuidarmais/MainActivity.java
@@ -1,8 +1,10 @@
 package cuidarmais.org.cuidarmais;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cuidarmais.org.cuidarmais.fragments.ClientFragment;
+import cuidarmais.org.cuidarmais.fragments.PadFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,25 +82,30 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
+        displayScreen(id);
+        return true;
+    }
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    public void displayScreen(int id){
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.clientprofile) {
+            fragment = new ClientFragment();
+        } else if (id == R.id.pad) {
+            fragment = new PadFragment();
+        }else if (id == R.id.note) {
 
-        } else if (id == R.id.nav_manage) {
+        }
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if(fragment != null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content_frame, fragment);
+            transaction.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
---
+++ app/src/main/java/cuidarmais/org/cuidarmais/fragments/ScheduleFragment.java
@@ -0,0 +1,37 @@
+package cuidarmais.org.cuidarmais.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cuidarmais.org.cuidarmais.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScheduleFragment extends Fragment {
+
+
+    public ScheduleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.schedule_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Schedule");
+    }
+}
