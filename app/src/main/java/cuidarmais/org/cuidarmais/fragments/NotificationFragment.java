4c645963e9d569dd0d9d18fad35895dea90afd38
Deco Oliveira <deco.oliveira@googlemail.com>
2017-09-20 10:50:39 -0300

    notification list adapter

--- app/src/main/java/cuidarmais/org/cuidarmais/fragments/NotificationFragment.java
+++ app/src/main/java/cuidarmais/org/cuidarmais/fragments/NotificationFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 import cuidarmais.org.cuidarmais.R;
+import cuidarmais.org.model.Notification;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +29,15 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View view =  inflater.inflate(R.layout.fragment_notification, container, false);
+        ListView notificationList = (ListView) view.findViewById(R.id.notificationList);
+        List<Notification> notifications = null;
+
+        ArrayAdapter<Notification> adapter = new ArrayAdapter<Notification>(getActivity(),
+                android.R.layout.simple_list_item_1, notifications);
+
+        notificationList.setAdapter(adapter);
+        return view;
     }
 
 }
---
+++ app/src/main/java/cuidarmais/org/model/Notification.java
@@ -0,0 +1,15 @@
+package cuidarmais.org.model;
+
+/**
+ * Created by deco on 20/09/17.
+ */
+
+public class Notification {
+
+    public Notification() {
+    }
+
+
+
+
+}
