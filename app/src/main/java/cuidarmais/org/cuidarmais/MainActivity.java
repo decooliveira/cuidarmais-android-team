81917829b1a83c1f72be6eb17dae25bff0a8da9f
Deco Oliveira <deco.oliveira@googlemail.com>
2017-09-20 09:59:44 -0300

    contact interface

--- app/src/main/java/cuidarmais/org/cuidarmais/MainActivity.java
+++ app/src/main/java/cuidarmais/org/cuidarmais/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 
 import cuidarmais.org.cuidarmais.fragments.AskEquipmentFragment;
 import cuidarmais.org.cuidarmais.fragments.AskMaterialFragment;
@@ -42,6 +44,19 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //method that hides soft keyboard when it loses focus
+        findViewById(R.id.drawer_layout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/cuidarmais/org/cuidarmais/fragments/ContactFragment.java
+++ app/src/main/java/cuidarmais/org/cuidarmais/fragments/ContactFragment.java
@@ -3,9 +3,12 @@ package cuidarmais.org.cuidarmais.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import cuidarmais.org.cuidarmais.R;
 
@@ -23,8 +26,16 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view  = inflater.inflate(R.layout.fragment_contact, container, false);
+        EditText messageText = (EditText) view.findViewById(R.id.messageTextView);
+        //Set keyboard settings to display DONE button with multiline text
+        messageText.setImeOptions(messageText.getImeOptions() | EditorInfo.IME_ACTION_DONE);
+        messageText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+
+        return  view;
     }
 
 }
