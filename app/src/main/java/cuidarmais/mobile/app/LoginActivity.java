f4869b1fd9e7e941a9f37ba667ee87761fe54e4d
Deco Oliveira <deco.oliveira@googlemail.com>
2018-02-11 16:59:30 -0200

    pass object into tab

--- app/src/main/java/cuidarmais/mobile/app/LoginActivity.java
+++ app/src/main/java/cuidarmais/mobile/app/LoginActivity.java
@@ -1,24 +1,10 @@
 package cuidarmais.mobile.app;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -26,8 +12,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -44,14 +28,12 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import cuidarmais.mobile.TokenSaver;
-
-import static android.Manifest.permission.READ_CONTACTS;
+import cuidarmais.mobile.service.API;
+import cuidarmais.mobile.service.TokenSaver;
+import cuidarmais.mobile.service.Connector;
 
 /**
  * A login screen that offers login via email/password.
@@ -80,8 +62,9 @@ public class LoginActivity extends AppCompatActivity  {
 
         String token = TokenSaver.getToken(getApplicationContext());
         Log.v(" STORED TOKEN", token);
-        if(!token.isEmpty()){
-            open();
+
+        if(validateToken(token)){
+            openApp();
         }
         requestQueue = Volley.newRequestQueue(this);
 
@@ -107,7 +90,7 @@ public class LoginActivity extends AppCompatActivity  {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    authenticate();
                     return true;
                 }
                 return false;
@@ -118,7 +101,7 @@ public class LoginActivity extends AppCompatActivity  {
         loginBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                authenticate();
             }
         });
 
@@ -136,9 +119,9 @@ public class LoginActivity extends AppCompatActivity  {
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
+    private void authenticate() {
 
-        String url = API.url+"/authenticate";
+        String url = API.url+"authenticate";
 
         String username = usernameView.getText().toString();
         String password = passwordView.getText().toString();
@@ -158,7 +141,7 @@ public class LoginActivity extends AppCompatActivity  {
 
                             String token = response.getString("id_token");
                             TokenSaver.setToken(getApplicationContext(),token);
-                            open();
+                            openApp();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -202,8 +185,19 @@ public class LoginActivity extends AppCompatActivity  {
         return password.length() > 4;
     }
 
+    private boolean validateToken(String token){
+        if(!token.isEmpty()){
 
-    private void open(){
+            Context context = getApplicationContext();
+            Connector connector = new Connector(context);
+            return connector.validateToken();
+
+
+        }
+        return false;
+    }
+
+    private void openApp(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
 
